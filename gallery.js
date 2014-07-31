2149bd6cdd04b463ff0f11cd2e65a9cd8270312c
Winter Wei <winterwei@gmail.com>
2014-07-31 18:52:10 -0400

    load imagePanel, cycle images, show/hide ccaptions, show/hide buttons in progress

--- gallery.js
+++ gallery.js
@@ -6,12 +6,57 @@ $(function(){
 	// 	$(this).css('display','none');
 	// });
 	var $imagePanel = $('.imagePanel');
-	var $image = $('.image');
+	var $image = $('article div');
+	var $caption = $('#caption');
+	var $toggleCaption = $('.toggleCaption');
+	var $back = $('.backButton');
+	var $next = $('.nextButton');
+	var $currentImage;
+
+
+	//
+	var changeBackgroundImage = function() {
+		var backgroundImage = $currentImage.css('background-image');
+		$imagePanel.css('background-image',backgroundImage);
+
+		//retrieve caption from data-caption and set it
+		//to #caption
+		var imageCap = $currentImage.data('caption');
+		$caption.text(imageCap).hide();
+		// if ($currentImage.hasClass('image1')){
+		// 	$back.hide();
+		// } else {
+		// 	$next.show();
+		// };
+		// if ($currentImage.is(':last-child')){
+		// 	$next.hide();
+		// } else {
+		// 	$back.show();
+		// };
+
+	};
 
 	$image.on('click', function(){
-		var backgroundImage = $(this).css('background-image');
-		$imagePanel.css('background-image',backgroundImage);
-		$(this).data('caption')
+		$currentImage = $(this);
+		//set the background image to corresponding image clicked
+
+		changeBackgroundImage();
+		//imagePanel appears when image clicked
 		$imagePanel.fadeIn('slow'); 
+	});
+
+
+	$toggleCaption.on('click', function(){
+		$caption.toggle('slow');
+	});
+
+	$back.on('click',function(){
+		$currentImage = $currentImage.prev();
+		changeBackgroundImage();
+	});
+
+	$next.on('click',function(){
+		$currentImage = $currentImage.next();
+		changeBackgroundImage();
 	});
 });
